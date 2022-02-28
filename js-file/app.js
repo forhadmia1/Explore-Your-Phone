72923cb2e11fa1836d65afb025c7da17632ca192
forhadmia1 <forhad8613@gmail.com>
2022-03-01 00:10:13 +0600

    search handle for 20 products

--- js-file/app.js
+++ js-file/app.js
@@ -5,6 +5,7 @@ const loadResult = () =>{
     const emptyError =document.getElementById('empty-field');
     if(searchKeyword==''){
         emptyError.style.display= "block";
+        document.getElementById('error').style.display="none";
     }else{
         emptyError.style.display= "none";
         fetch(` https://openapi.programming-hero.com/api/phones?search=${searchKeyword}`)
@@ -20,11 +21,18 @@ const displayData=phones=>{
     document.getElementById('single-details').textContent='';
     phoneContainer.textContent='';
     //show result
+    let numberOfPhones;
     if(phones.length == 0){
         errorContainer.style.display='block';
     }else{
+        if(phones.length>20){
+            numberOfPhones= phones.slice(0,20);
+        }else{
+            numberOfPhones=phones;
+        };
+        //display result in UI
         errorContainer.style.display = 'none';
-        phones.forEach(phone => {
+        numberOfPhones.forEach(phone => {
             const div = document.createElement('div');
             div.className ="col"
             div.innerHTML= `
